53e53f02edee3f7b8828494784f96a0f74771b6f
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-11-20 23:20:41 +0600

    login added

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,28 @@
+<?php
+require_once './dbcon.php';
+session_start();
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $username_check = mysqli_query($link, "SELECT * FROM `users` WHERE `username` = '$username'");
+    if (mysqli_num_rows($username_check) > 0) {
+        $row = mysqli_fetch_assoc($username_check);
+        if ($row['password'] == md5($password)) {
+            if ($row['status'] == 'active') {
+                header('location:index.php');
+            } else {
+                $status_inactive = "Your Status Inactive.";
+            }
+        } else {
+            $wrong_password = "This Password Wrong";
+        }
+    } else {
+        $username_not_found = "This User Name Not Found";
+    }
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -21,11 +46,15 @@
                 <h2 class="text-center">Admin Login Form</h2>
                 <form action="login.php" method="POST">
                     <div>
-                        <input type="text" placeholder="Username" name="username" required="" class="form-control">
+                        <input type="text" placeholder="Username" name="username" required="" class="form-control" value="<?php if (isset($username)) {
+                                                                                                                                echo $username;
+                                                                                                                            } ?>">
                     </div>
                     <br>
                     <div>
-                        <input type="password" placeholder="Password" name="password" required="" class="form-control">
+                        <input type="password" placeholder="Password" name="password" required="" class="form-control" value="<?php if (isset($password)) {
+                                                                                                                                    echo $password;
+                                                                                                                                } ?>">
                     </div>
                     <br>
                     <div>
@@ -35,6 +64,22 @@
                 </form>
             </div>
         </div>
+        <?php
+        if (isset($username_not_found)) {
+            echo '<div class="alert alert-danger col-sm-4" style="margin-left: 425px; margin-top:40px;">' . $username_not_found . '  </div>';
+        }
+        ?>
+        <?php
+        if (isset($wrong_password)) {
+            echo '<div class="alert alert-danger col-sm-4" style="margin-left: 425px; margin-top:40px;">' . $wrong_password . '  </div>';
+        }
+        ?>
+        <?php
+        if (isset($status_inactive)) {
+            echo '<div class="alert alert-danger col-sm-4" style="margin-left: 425px; margin-top:40px;">' . $status_inactive . '  </div>';
+        }
+        ?>
+
     </div>
 
 </body>
