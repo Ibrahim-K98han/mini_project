8a0ac1ea2aaa0a51853a503f96720abfe845f674
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-11-21 22:05:43 +0600

    somthing file added

---
+++ admin/dashboard.php
@@ -0,0 +1,126 @@
+<h1 class="text-primary">Dashboard <small style="color:gray">Statics Overview</small></h1>
+<ol class="breadcrumb bg-light p-2">
+    <li class="active">Dashboard</li>
+</ol>
+<div class="row">
+    <div class="col-sm-4">
+        <div class="bg-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <img src="../images/user.jpg" alt="">
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="pull-right">
+                            <h2 style="font-size: 45px; color:#fff; margin-left:200px;">10</h2>
+                        </div>
+                        <div class="pull-right">
+                            <p style="margin-left: 200px; color:#fff;"> All Student</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <a href="" style="text-decoration: none;">
+            <div class="panel-footer" style="border: 1px solid blue; padding:10px;">
+                <span class="pull-left">All Student</span>
+                <span class="pull-right"><i class="fa fa-arrow-cirlce-o-right"></i> </span>
+            </div>
+        </a>
+    </div>
+    <div class="col-sm-4">
+        <div class="bg-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <img src="../images/user.jpg" alt="">
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="pull-right">
+                            <h2 style="font-size: 45px; color:#fff; margin-left:200px;">10</h2>
+                        </div>
+                        <div class="pull-right">
+                            <p style="margin-left: 200px; color:#fff;"> All Student</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <a href="" style="text-decoration: none;">
+            <div class="panel-footer" style="border: 1px solid blue; padding:10px;">
+                <span class="pull-left">All Student</span>
+                <span class="pull-right"><i class="fa fa-arrow-cirlce-o-right"></i> </span>
+            </div>
+        </a>
+    </div>
+    <div class="col-sm-4">
+        <div class="bg-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <img src="../images/user.jpg" alt="">
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="pull-right">
+                            <h2 style="font-size: 45px; color:#fff; margin-left:200px;">10</h2>
+                        </div>
+                        <div class="pull-right">
+                            <p style="margin-left: 200px; color:#fff;"> All Student</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <a href="" style="text-decoration: none;">
+            <div class="panel-footer" style="border: 1px solid blue; padding:10px;">
+                <span class="pull-left">All Student</span>
+                <span class="pull-right"><i class="fa fa-arrow-cirlce-o-right"></i> </span>
+            </div>
+        </a>
+    </div>
+</div>
+<hr>
+<h3>New Students</h3>
+<div class="table-responsive">
+    <table id="data" class="table table-hover table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Roll</th>
+                <th>City</th>
+                <th>Contact</th>
+                <th>Photo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Ibrahim Khan</td>
+                <td>359896</td>
+                <td>Dhaka</td>
+                <td>01751800957</td>
+                <td><img style="width: 80px;" src="../images/user.jpg" alt=""></td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Ibrahim Khan</td>
+                <td>359896</td>
+                <td>Dhaka</td>
+                <td>01751800957</td>
+                <td><img style="width: 80px;" src="../images/user.jpg" alt=""></td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Ibrahim Khan</td>
+                <td>359896</td>
+                <td>Dhaka</td>
+                <td>01751800957</td>
+                <td><img style="width: 80px;" src="../images/user.jpg" alt=""></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- admin/index.php
+++ admin/index.php
@@ -4,4 +4,82 @@ if (!isset($_SESSION['user_login'])) {
     header('location:login.php');
 }
 ?>
-<a href="logout.php">Logout</a>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/fontawesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/dataTables.bootstrap4.min.css">
+
+    <script type="text/javascript" src="../js/jquery-3.5.1.js"></script>
+    <script type="text/javascript" src="../js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="../js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="../js/script.js"></script>
+
+
+    <title>SMS</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="color:gray">SMS</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content: flex-end;">
+                <ul class="nav navbar-right col-sm-5">
+                    <li><a style="text-decoration:none; color:gray" href="logout.php"><i class="fa fa-user"></i> Welcome: Ibrahim Khan</a></li>
+                    <li style="margin-left: 20px;"><a style="text-decoration:none; color:gray" href="logout.php"><i class="fa fa-user-plus"></i> Add User</a></li>
+                    <li style="margin-left: 20px;"><a style="text-decoration:none; color:gray" href="logout.php"><i class="fa fa-user"></i> Profile</a></li>
+                    <li style="margin-left: 20px;"><a style="text-decoration:none; color:gray" href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="list-group">
+                    <a href="index.php?page=dashboard" class="list-group-item list-group-item-action active" aria-current="true">
+                        Dashboard
+                    </a>
+                    <a href="index.php?page=add-student" class="list-group-item list-group-item-action">Add Student</a>
+                    <a href="index.php?page=all-student" class="list-group-item list-group-item-action">All Student</a>
+                    <a href="index.php?page=all-users" class="list-group-item list-group-item-action">All Users</a>
+                </div>
+            </div>
+            <div class="col-sm-9">
+                <div class="content" style="min-height: 700px;">
+                    <?php
+
+
+                    if (isset($_GET['page'])) {
+                        $page = $_GET['page'] . '.php';
+                    } else {
+                        $page = "dashboard.php";
+                    }
+
+                    if (file_exists($page)) {
+                        require_once $page;
+                    } else {
+                        echo '<h1>File Not Found</h1>';
+                    }
+
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer-area" style="background: #3CA9EB; text-align:center; padding:20px 0; color:#fff;">
+        <p style="margin: 0;">Copyright &copy; 2021 Student Management System. All Right Are Reserved</p>
+    </footer>
+</body>
+
+</html>
